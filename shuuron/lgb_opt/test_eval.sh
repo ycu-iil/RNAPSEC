0b4cbc1cf6f0816c07764a2620b49fa75220842c
cccchin <kynjpn@gmail.com>
2023-08-17 14:55:32 +0900

    delete old version

--- shuuron/lgb_opt/test_eval.sh
+++
@@ -1,9 +0,0 @@
-# python cutdata.py
-# python cutdata_opt.py
-python roc_curve.py
-python pr_curve.py
-python confusion_matrix.py
-python calc_score_pr.py
-python table8_all_evaluation_score.py
-python feature_importances.py
-python fig11ab_kfold_roc.py
--- shuuron/smote_lgb/test_eval.sh
+++
@@ -1,9 +0,0 @@
-# python cutdata.py
-# python train_val_smote_opt.py
-python roc_curve.py
-python pr_curve.py
-python confusion_matrix.py
-python feature_importances.py
-python calc_score_pr.py
-python table8_all_evaluation_score.py
-
