600b6cbfed73de52bcd6275732f88b9507017dec
cccchin <kynjpn@gmail.com>
2024-01-15 16:30:44 +0900

    path

--- predict_behavior_new_sequences/prepro_input/run_mf.sh
+++ predict_behavior_new_sequences/prepro_input/run_mf.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-python /home/chin/download/MathFeature/methods/ExtractionTechniques.py -i ./prepro_results/rna_seq.fasta -o ./mf_result/mf_3mer.csv -t kmer -seq 2 < mf_input.txt
-python /home/chin/download/MathFeature/methods/FickettScore.py -i ./prepro_results/rna_seq.fasta -o ./mf_result/mf_fickett.csv -seq 2
-python /home/chin/download/MathFeature/methods/CodingClass.py -i ./prepro_results/rna_seq.fasta -o ./mf_result/mf_orf.csv 
-python /home/chin/download/MathFeature/methods/TsallisEntropy.py -i ./prepro_results/rna_seq.fasta -o ./mf_result/mf_tsallis_entropy.csv -k 2 -q 0.1
-python /home/chin/download/MathFeature/methods/EntropyClass.py -i ./prepro_results/rna_seq.fasta -o ./mf_result/mf_shannon.csv -k2 -e Shannon
-python /home/chin/download/MathFeature/methods/FourierClass.py -i ./prepro_results/rna_seq.fasta -o ./mf_result/mf_fourier_binary.csv -r 1
-python /home/chin/download/MathFeature/methods/FourierClass.py -i ./prepro_results/rna_seq.fasta -o ./mf_result/mf_fourier_zcurve.csv -r 2
-python /home/chin/download/MathFeature/methods/FourierClass.py -i ./prepro_results/rna_seq.fasta -o ./mf_result/mf_fourier_real.csv -r 3
+python ~/MathFeature/methods/ExtractionTechniques.py -i ./prepro_results/rna_seq.fasta -o ./mf_result/mf_3mer.csv -t kmer -seq 2 < mf_input.txt
+python ~/MathFeature/methods/FickettScore.py -i ./prepro_results/rna_seq.fasta -o ./mf_result/mf_fickett.csv -seq 2
+python ~/MathFeature/methods/CodingClass.py -i ./prepro_results/rna_seq.fasta -o ./mf_result/mf_orf.csv 
+python ~/MathFeature/methods/TsallisEntropy.py -i ./prepro_results/rna_seq.fasta -o ./mf_result/mf_tsallis_entropy.csv -k 2 -q 0.1
+python ~/MathFeature/methods/EntropyClass.py -i ./prepro_results/rna_seq.fasta -o ./mf_result/mf_shannon.csv -k2 -e Shannon
+python ~/MathFeature/methods/FourierClass.py -i ./prepro_results/rna_seq.fasta -o ./mf_result/mf_fourier_binary.csv -r 1
+python ~/MathFeature/methods/FourierClass.py -i ./prepro_results/rna_seq.fasta -o ./mf_result/mf_fourier_zcurve.csv -r 2
+python ~/MathFeature/methods/FourierClass.py -i ./prepro_results/rna_seq.fasta -o ./mf_result/mf_fourier_real.csv -r 3
 
 
 
